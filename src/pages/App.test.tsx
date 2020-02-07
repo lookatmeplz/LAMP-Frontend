7f024a9d47b7038e4836748631f8f8b37ed0b531
HyeockJinKim <kherootz@gmail.com>
2020-02-08 00:11:17 +0900

    Calender와 App 수정

--- src/pages/App.test.tsx
+++ src/pages/App.test.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders h2', () => {
   const { getByText } = render(<App />);
   const linkElement1 = getByText(/년/i);
   const linkElement2 = getByText(/월/i);
   const linkElement3 = getByText(/일/i);
+
+  expect(linkElement1).toBeInTheDocument();
+  expect(linkElement2).toBeInTheDocument();
+  expect(linkElement3).toBeInTheDocument();
+});
+
+test('renders h4', () => {
+  const { getByText } = render(<App />);
+  const linkElement1 = getByText(/시/i);
+  const linkElement2 = getByText(/분/i);
+  const linkElement3 = getByText(/초/i);
 
   expect(linkElement1).toBeInTheDocument();
   expect(linkElement2).toBeInTheDocument();
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Calender from './components/Calender'
+import Calender from '../components/Calender'
+import TimeLine from "../components/TimeLine";
 
 const App = () => {
   return (
     <div className="App">
       <Calender />
+      <TimeLine />
     </div>
   );
 };
